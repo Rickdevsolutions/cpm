01b455a736dc436a406c7b3cc64ba714da79e569
Rick <rickdevsolutions@gmail.com>
2025-05-23 18:16:20 -0300

    Update cpmcheats.py

--- cpmcheats.py
+++ cpmcheats.py
@@ -4,7 +4,7 @@ import requests
 import subprocess
 import urllib.parse
 
-__ENDPOINT_URL__: str = "https://cpmcheats.abccloud.com.br/api2"
+__ENDPOINT_URL__: str = "https://cpmcheats.hostzera.com.br/api2"
 
 class CPMCheats:
     def __init__(self, access_key) -> None:
